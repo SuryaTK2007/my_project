796685eff126279f279f377c54ddef4121c22817
surya <suryatk2007@gmail.com>
2025-05-10 02:59:58 +0530

    included dependencies in cargo.toml (rand="0.8.5") and in main.rs used that rand::Rng and implemented thread_rng().gen_range(1..=100)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
-fn main() {
-    println!("Enter a number");
-    let mut guess = String::new();
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("This line has no error");
-    println!("You entered {}", guess);
+use rand::Rng;
+fn main(){
+    let secret=rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secret}");
+    let mut guess=String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read your input");
+    println!("Your guess is {guess}");
 }
