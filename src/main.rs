d8dab138feb38cfe726e5991f540c9829aa413ff
surya <suryatk2007@gmail.com>
2025-05-11 02:41:19 +0530

    Number guessing game in rust created using loop, cmp, Ordering, match, trim, parse.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main(){
     let secret=rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {secret}");
-    let mut guess=String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read your input");
-    println!("Your guess is {guess}");
+    loop{
+        println!("Guess the Number: ");
+        let mut guess=String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>continue,
+        };
+        
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret){
+            Ordering::Less=>println!("Too low"),
+            Ordering::Greater=>println!("Too big!"),
+            Ordering::Equal=>{
+                println!("You won!");
+                break;
+            }
+        }
+    }
 }
