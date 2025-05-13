066b89e41454b9bfc2c972ccc50dea9d79126b15
surya <suryatk2007@gmail.com>
2025-05-13 17:59:36 +0530

    Learned about scope block and there is no need for ';' to return a value in the block

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::io;
 fn main(){
-    let arr=[3; 5];
-    let mut index=String::new();
-    io::stdin().read_line(&mut index).expect("Can't read the line");
-    let index: usize = index.trim().parse().expect("Can't convert it to type u32");
-    println!("The value of element at the {index} is {}",arr[index]);
+    let y={
+        let x=5;
+        x+1
+    };
+    println!("The value of y is {y}");
 }
