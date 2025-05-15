d917b776a6fc210b3aab620c8fb7b108e976f1e2
surya <suryatk2007@gmail.com>
2025-05-16 00:06:38 +0530

    In this code, we used loop in a statement and using loop we're returning value and break statement is used

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main(){
-    loop{
-        println!("hello");
-    }
+    let mut counter=0;
+    let result=loop{
+        counter+=1;
+        if counter==10{
+            break counter*2
+        }
+    };
+    println!("{result}");
 }
  
  
