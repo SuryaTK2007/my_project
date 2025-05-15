207a6c405287a466d04bcdb4aa71db425e3de4bf
surya <suryatk2007@gmail.com>
2025-05-15 23:52:51 +0530

    Normal if else

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-//This is comment
-//There is no multi line comment in rust
-use std::io;
-fn add(x: i32, y: i32)->i32{
-    x+y
-}
 fn main(){
-    let mut x=String::new();
-    let mut y=String::new();
-    println!("Enter the value of x");
-    io::stdin().read_line(&mut x).expect("Failed to read the line");
-    println!("Enter the value of y");
-    io::stdin().read_line(&mut y).expect("Failed to read the line");
-    let x: i32= x.trim().parse().expect("Can't parse into i32");
-    let y: i32= y.trim().parse().expect("Can't parse into i32");
-    let sum=add(x,y);
-    println!("The sum of x and y is {sum}");
+    let number=3;
+    if number!=3 {
+        println!("It is not a three");
+    }
+    else{
+        println!("It is three");
+    }
 }
+ 
