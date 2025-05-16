1892eab03d6ec3d58de3889fc14fbe65b282b538
surya <suryatk2007@gmail.com>
2025-05-16 19:55:32 +0530

    Traversing an array using for loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main(){
     let arr=[1,2,3,4];
-    let mut index=0;
-    while index<4{
-        println!("{}",arr[index]);
-        index+=1;
+    for element in arr{
+        println!("{element}");
     }
 }
