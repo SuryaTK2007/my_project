f6b5f7f49b8a1569d7a58a88279e7cb981386432
surya <suryatk2007@gmail.com>
2025-05-19 04:42:58 +0530

    mutating a string is possible

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main(){
-    let s1=String::from("hello!");
-    let s2=s1;
-    println!("{s1}, world");
+    let mut s = String::from("hello");
+    s = String::from("ahoy");
+    println!("{s}, world!");
+
 }
