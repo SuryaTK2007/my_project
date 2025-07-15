13d639af0e2b8ac15f9552ee29589686d8180c8d
Surya <suryatk2007@gmail.com>
2025-07-15 19:37:49 +0530

    It is the safest way to get input from the user using match because we have full control over the data with Ok and Err

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::io;
 fn main(){
     let mut buf=String::new();
-    io::stdin().read_line(&mut buf).unwrap();
-    let mut n1:i32=buf.trim().parse().unwrap();
+    io::stdin().read_line(&mut buf).expect("Failed to read input");
+    let n1_result=buf.trim().parse::<i32>();
+    let n1=match n1_result{
+        Ok(val)=>val,
+        Err(_)=>{
+            println!("Failed to read first number");
+            return;
+        }
+    };
     buf.clear();
-    io::stdin().read_line(&mut buf).unwrap();
-    let mut n2:i32=buf.trim().parse().unwrap();
-    println!("The sum of {} and {} is {}", n1,n2,n1+n2);
+    io::stdin().read_line(&mut buf).expect("Failed to read input");
+    let n2_result=buf.trim().parse::<i32>();
+    let n2=match n2_result{
+        Ok(val)=>val,
+        Err(_)=>{
+            println!("Failed to read second number");
+            return;
+        }
+    };
+    println!("The sum of {} and {} is {}",n1,n2,n1+n2);
 }
