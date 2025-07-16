91bc1edbedf8d82097c8be4501a65c9529b39ebe
Surya <suryatk2007@gmail.com>
2025-07-16 20:35:18 +0530

    prime number checking program

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use std::io;
+fn is_prime(n:i32)->bool{
+    for i in 2..=((n as f64).sqrt() as i32){
+        if n%i==0{
+            return false;
+        }
+    }
+    return true;
+}
 fn main(){
     let mut buf=String::new();
-    io::stdin().read_line(&mut buf).expect("Failed to read input");
+    io::stdin().read_line(&mut buf).expect("Can't read the input");
     let n1_result=buf.trim().parse::<i32>();
     let n1=match n1_result{
         Ok(val)=>val,
         Err(_)=>{
-            println!("Failed to read first number");
+            println!("Invalid input for number");
             return;
         }
     };
-    buf.clear();
-    io::stdin().read_line(&mut buf).expect("Failed to read input");
-    let n2_result=buf.trim().parse::<i32>();
-    let n2=match n2_result{
-        Ok(val)=>val,
-        Err(_)=>{
-            println!("Failed to read second number");
-            return;
-        }
-    };
-    println!("The sum of {} and {} is {}",n1,n2,n1+n2);
+    if n1==1{
+        println!("Not a prime number");
+        return;
+    }
+    let x:bool=is_prime(n1);
+    if x{
+        println!("The number is prime number!");
+    }
+    else{
+        println!("The number is not a prime number!");
+    }
 }
