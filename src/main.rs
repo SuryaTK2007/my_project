c02b281023c66c06eb4c72da0f7047af4f365585
Surya <suryatk2007@gmail.com>
2025-07-19 09:27:56 +0530

    vector program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+use std::io;
 fn main(){
-    for i in 1..11{
-        print!("{} ",i);
+    let mut buf=String::new();
+    println!("Enter the size of array: ");
+    io::stdin().read_line(&mut buf).expect("Error in reading input");
+    let n1_result=buf.trim().parse::<i32>();
+    let n1=match n1_result{
+        Ok(val)=>val,
+        Err(_)=>{
+            println!("Invalid input for n1");
+            return;
+        }
+    };
+    buf.clear();
+    let mut v=Vec::new();
+    println!("Enter the elemets of array: ");
+    for i in 0..n1{
+        io::stdin().read_line(&mut buf).expect("Error in reading input");
+        let x_result=buf.trim().parse::<i32>();
+        let x=match x_result{
+            Ok(val)=>val,
+            Err(_)=>{
+                println!("Invalid input for array element");
+                return;
+            }
+        };
+        v.push(x);
+        buf.clear();
+    }
+    for i in 0..v.len(){
+        print!("{} ",v[i]);
     }
 }
